76ca47a293a003b04099d939ac2d5c25666e5358
SokolovS <ierehon@mail.ru>
2023-01-27 18:31:30 +0400

    Задание 3

--- src/Cars.java
+++ src/Cars.java
@@ -1,20 +1,36 @@
+import java.util.Objects;
+
 public class Cars {
     private String brand;
     private String model;
-    private String engineVolume;
+    private double engineVolume;
     private String color;
-    private String year;
+    private int year;
     private String country;
 
-
-    public Cars(String brand, String model, String engineVolume, String color, String year, String country) {
+    public Cars(String brand, String model, double engineVolume, String color, int year, String country) {
         this.brand = brand;
         this.model = model;
         this.engineVolume = engineVolume;
         this.color = color;
         this.year = year;
         this.country = country;
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        }
+        if (color == null || color.length() == 0) {
+            this.color = "white";
+        }
+        if (brand == null || model == null || country == null || brand.length() == 0 || model.length() == 0 || country.length() == 0) {
+            this.brand = "default";
+            this.model = "default";
+            this.country = "default";
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        }
     }
+
 
     public String getBrand() {
         return brand;
@@ -32,11 +48,11 @@ public class Cars {
         this.model = model;
     }
 
-    public String getEngineVolume() {
+    public double getEngineVolume() {
         return engineVolume;
     }
 
-    public void setEngineVolume(String engineVolume) {
+    public void setEngineVolume(double engineVolume) {
         this.engineVolume = engineVolume;
     }
 
@@ -48,11 +64,11 @@ public class Cars {
         this.color = color;
     }
 
-    public String getYear() {
+    public int getYear() {
         return year;
     }
 
-    public void setYear(String year) {
+    public void setYear(int year) {
         this.year = year;
     }
 
@@ -66,13 +82,16 @@ public class Cars {
 
     @Override
     public String toString() {
-        return "Car{" +
-                "Марка='" + brand + '\'' +
-                ",  Модель='" + model + '\'' +
-                ", Обьёь двигателя в л=" + engineVolume +
-                ", Цвет='" + color + '\'' +
-                ", Год выпуска=" + year +
-                ", Страна сборки='" + country + '\'' +
+        return "Cars{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + String.format("%.2f", engineVolume) +
+                ", color='" + color + '\'' +
+                ", year='" + year + '\'' +
+                ", country='" + country + '\'' +
                 '}';
+
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Cars ladaGranta = new Cars("Lada", "Granta", "1.7", "Yellow",
-                "2015", "Russa");
-        Cars audiA8 = new Cars("Audi", "A8 50L TDI quattro", "3.0", "Black",
-                "2020", "Germany");
-        Cars BMWZ8 = new Cars("BMW", "Z8", "3.0", "Black",
-                "2021", "Germany");
-        Cars kiaSportage4Th = new Cars("Kia", "Sportage 4th generation", "2.4",
-                "Red", "2018", "South korea");
-        Cars hyundaiAvante = new Cars("Hyundai", "Avante", "1.6",
-                "Orange", "2016", "South korea");
+        Cars ladaGranta = new Cars("", "0", -1, "",
+                0, "");
+        Cars audiA8 = new Cars("Audi", "A8 50L TDI quattro", 3.0, "Black",
+                2020, "Germany");
+        Cars BMWZ8 = new Cars("BMW", "Z8", 3.0, "Black",
+                2021, "Germany");
+        Cars kiaSportage4Th = new Cars("Kia", "Sportage 4th generation", 2.4,
+                "Red", 2018, "South korea");
+        Cars hyundaiAvante = new Cars("Hyundai", "Avante", 1.6,
+                "Orange", 2016, "South korea");
         System.out.println(ladaGranta);
         System.out.println(audiA8);
         System.out.println(BMWZ8);
         System.out.println(kiaSportage4Th);
         System.out.println(hyundaiAvante);
+
     }
 }
